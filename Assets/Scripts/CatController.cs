c0ae90fe73cd0524b4d487db15d4d621d4b8ce43
TWITexe <kawaii_senpaii@mail.ru>
2023-03-07 22:28:44 +0700

    Minor bug fix
    Fixed a bug due to which the main character could not move towards the target set by the target  commands (!cathome, !catchair)

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -6,32 +6,32 @@ public class CatController : MonoBehaviour
     // Скрипт банка сообщений в чате
     [SerializeField] private VariableStorage chatMessage;
     // Главный герой
-    [SerializeField] private GameObject cat;     
+    [SerializeField] private GameObject cat;
+    private bool catReady = true;
     // Аниматор TV
     [SerializeField] private Animator tvAnim;
     // Аниматор кота
     [SerializeField] private Animator catsAnim;                                                      
     // Начальная точка кота при движении
-    private Vector3 _startPos = new Vector3(5.3f, -2f, 0);                    
+    private Vector3 _defaultStartPos = new Vector3(5.3f, -2f, 0);
+    private Vector3 _startPos;                    
     // Конечная точка кота при движении
-    private Vector3 _endPos;                                                       
+    private Vector3 _endPos = new Vector3(5.3f, -2f, 0);                                                       
     // Кошачий дом
     [SerializeField] private Animator catsHome;                                            
 
     private void Start()
     {
-        catsAnim = cat.GetComponent<Animator>();
+        _startPos = _defaultStartPos;
     }
 
     private void Update()
     {
         Debug.Log("message = " + chatMessage.Message);
         Debug.Log("name = " + chatMessage.ChatName);
-        if (_endPos == _startPos)
-            cat.GetComponent<SpriteRenderer>().flipX = false;
-        
-        // Если есть !tv в команде, проверяет дальше, если нет, не проходится по остальным if'ам
-        if (chatMessage.Message.ToLower().IndexOf("!tv") > -1)               
+
+        // Если есть "!tv" в чате, проверяет дальше, если нет, не проходится по остальным if'ам
+        if (chatMessage.Message.ToLower().LastIndexOf("!tv") > -1)               
         {
             switch (chatMessage.Message.ToLower())
             {
@@ -62,35 +62,43 @@ public class CatController : MonoBehaviour
             {
 
                 case "!cathome":
-                    if (_startPos == new Vector3(5.3f, -2f, 0) == false)
+                    if (_startPos != _defaultStartPos && catReady)
                     {
-                        ReadyToStart();  
+                        ReadyToStart();
                     }
-                    else
-                    {
+                    else if (_startPos == _defaultStartPos && catReady)
+                    {   
+                        //Кот не готов к следующим имзенениям, пока не достигнет цели.
+                        catReady = false;
+                        cat.SetActive(true);
                         CatLetsGo(new Vector3(-1.48f, -1.71f, 0), 2.5f);
                         if (_startPos == _endPos)
                         {
                             catsAnim.SetBool("isJump", false);
+                            catsHome.SetBool("isHome", true);
                             cat.SetActive(false);
-                            catsHome.GetComponent<Animator>().SetBool("isHome", true);
+                            catReady = true;
                         }
                     }
 
                     break;
                 case "!catchair":
 
-                    if (_startPos == new Vector3(5.3f, -2f, 0) == false)
+                    if (_startPos != _defaultStartPos && catReady)
                     {
-                        ReadyToStart();  
+                        ReadyToStart();
                     }
-                    else
-                    {
+                    else if (_startPos == _defaultStartPos && catReady)
+                    {   
+                        //Кот не готов к следующим имзенениям, пока не достигнет цели.
+                        catReady = false;
+                        cat.SetActive(true);
                         CatLetsGo(new Vector3(3f, -1.3f, 0), 2.5f);
                         if (_startPos == _endPos)
                         {
                             catsAnim.SetBool("isJump", false);
-                            catsHome.SetBool("isChair", true);
+                            catsAnim.SetBool("isChair", true);
+                            catReady = true;
                         }
                     }
                     break;
@@ -101,10 +109,12 @@ public class CatController : MonoBehaviour
     private void ReadyToStart()                     // Стартовая позиция перед всеми действиями
     {
         Debug.Log("go ready");
-        cat.SetActive(true);
+        catsHome.SetBool("isHome", false);
+        
         CatLetsGo(new Vector3(5.3f, -2f, 0), 2.0f);
         cat.GetComponent<SpriteRenderer>().flipX = true;
         cat.transform.position = Vector3.MoveTowards (_startPos, _endPos, Time.deltaTime*2.5f);
+
     }
     private void CatLetsGo(Vector3 endPos, float speed)
     {
